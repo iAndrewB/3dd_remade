2bcdd151e3493994e6f52de0ba6fd19fcf7afe44
André Barros <andreparticular1@hotmail.com>
2018-04-18 17:27:00 +0100

    Added session information to topbar on dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -5,20 +5,23 @@
     <title>3DD - Dashboard</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="assets/css/main.min.css" rel="stylesheet" />
+    <link href="assets/css/main.css" rel="stylesheet" />
 </head>
 
 <body>
-    <div class="topnav">
+    <div class="topbar">
         <img class="logo" src="assets/img/dhl_logo.png" alt="DHL Logo" />
         <div class="sessioninfo">
-            <p class="sessioninfotitle">Logged in as:</p>
-            <p class="sessioninfotitle">Facility Name:</p>
-            <p class="sessioninfotitle">Workspace Name:</p>
+            <p class="sessioninfotitle">Logged in as:&nbsp;<?php echo "<span class='sessioninfodata'>LX2</span>"?></p>
+            <p class="sessioninfotitle">Facility Name:&nbsp;<?php echo "<span class='sessioninfodata'>LIS</span>"?></p>
+            <p class="sessioninfotitle">Workspace Name:&nbsp;<?php echo "<span class='sessioninfodata'>MGCOST</span>"?>&nbsp;&nbsp;<span class="sessioninfodata"><a href="#">Logout</a></span></p>
+            
+
         </div>
+        
     </div>
     <div class="sidenav">
-        <a href="#" class="tablinks" onclick="openTab(event, 'convertShipping')">»&nbsp;Converter Envio</a>
+        <a href="#" class="tablinks active" onclick="openTab(event, 'convertShipping')">»&nbsp;Converter Envio</a>
         <a href="#" class="tablinks" onclick="openTab(event, 'deleteConversion')">»&nbsp;Apagar Conversão</a>
         <a href="#" class="tablinks" onclick="openTab(event, 'updateConversion')">»&nbsp;Atualizar Conversão</a>
         <a href="#" class="tablinks" onclick="openTab(event, 'getConversions')">»&nbsp;Lista de Conversões</a>
@@ -26,7 +29,7 @@
     </div>
 
     <div class="content">
-        <div id="convertShipping" class="tabcontent active">
+        <div id="convertShipping" class="tabcontent" style="display: block;">
             <?php include_once( "templates/containers/convertShipping.php"); ?>
         </div>
         <div id="deleteConversion" class="tabcontent">
@@ -42,8 +45,7 @@
             <?php include_once( "templates/containers/logConversions.php"); ?>
         </div>
     </div>
-
+    
 </body>
-<script src="assets/js/main.min.js"></script>
-
+<script src="assets/js/main.js"></script>
 </html>
