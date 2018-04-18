fdd02d68be44a77a42b35c61a32e33b611a2d148
André Barros <andreparticular1@hotmail.com>
2018-04-18 17:29:43 +0100

    Created directory /templates/containers and with the files convertShipping/deleteConversion/updateConversion.php to allow dashboard.php content to be dynamic

---
+++ templates/containers/updateConversion.php
@@ -0,0 +1,85 @@
+<form>
+    <div class="row">
+        <h1>Procurar Envio</h1>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_pieceID">Número de Peça*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_pieceID" placeholder="Ex: JJD1020202102192192929">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+        </div>
+        <div class="col-input">
+            <button type='button' class='search_PieceID' style="float: right;">Procurar</button>
+        </div>
+    </div>
+</form>
+<hr>
+<form>
+    <div class="row">
+        <div class="col-title">
+            <h1>Dados de Envio</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataAddress">Morada*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataAddress" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataZIP">Código Postal*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataZIP" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataTown">Localidade*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataTown" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataPhone">Telemóvel*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataPhone" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataPieces">Número de Peças*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataPieces" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+            <label for="shipment_dataDuty">DUTY*: </label>
+        </div>
+        <div class="col-input">
+            <input type="text" id="shipment_dataDuty" placeholder="">
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-label">
+        </div>
+        <div class="col-input">
+            <button type='button' class='update_Conversion' style="float: right;">Atualizar</button>
+        </div>
+    </div>
+</form>
+
+
