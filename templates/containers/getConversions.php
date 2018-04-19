a12cae486d9d420b4369437e085e6e084b011388
André Barros <andreparticular1@hotmail.com>
2018-04-19 11:48:56 +0100

    Added tables to logConversions/getConversions.php and added DataTables Framework to style and add functionality to them

---
+++ templates/containers/getConversions.php
@@ -0,0 +1,26 @@
+<!-- START getConversions container -->
+<table id="conversionsTable" class="table">
+    <thead>
+        <tr>
+            <th>Login</th>
+            <th>Worskspace</th>
+            <th>Express AWB</th>
+            <th>Express PID</th>
+            <th>Parcel WB</th>
+            <th>Parcel PID</th>
+            <th>At</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>LX2</td>
+            <td>MGCOST</td>
+            <td>8443246792</td>
+            <td>JJ00084269500035921759</td>
+            <td>7160104730</td>
+            <td>JJD00016718060104730002</td>
+            <td>13/04/2018 14:58:00</td>
+        </tr>
+    </tbody>
+</table>
+<!-- END getConversions container -->
---
+++ templates/containers/logConversions.php
@@ -0,0 +1,28 @@
+<!-- START logConversions container -->
+<table id="actionsTable" class="table">
+    <thead>
+        <tr>
+            <th>Login</th>
+            <th>Worskspace</th>
+            <th>Action</th>
+            <th>Express AWB</th>
+            <th>Express PID</th>
+            <th>Parcel WB</th>
+            <th>Parcel PID</th>
+            <th>At</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>LX2</td>
+            <td>MGCOST</td>
+            <td>Convert</td>
+            <td>8443246792</td>
+            <td>JJ00084269500035921759</td>
+            <td>7160104730</td>
+            <td>JJD00016718060104730002</td>
+            <td>13/04/2018 14:58:00</td>
+        </tr>
+    </tbody>
+</table>
+<!-- END logConversions container -->
